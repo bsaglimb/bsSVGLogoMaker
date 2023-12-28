6faeb78bca51a3d4dc3099031365618031888647
bsaglimb <bsaglimb>
2023-12-28 00:50:56 -0500

    added then if statement

--- index.js
+++ index.js
@@ -77,3 +77,17 @@ function promptUser() {
             name: "shapeBackgroundColor",
           },
         ])
+        .then((answers) => {
+            // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+            if (answers.text.length > 3) {
+              console.log("Must enter a value of no more than 3 characters");
+              promptUser();
+            } else {
+              // Calling write file function to generate SVG file
+              writeToFile("logo.svg", answers);
+            }
+          });
+      }
+      
+      // Calling promptUser function so inquirer prompts are executed when application is ran
+      promptUser();
