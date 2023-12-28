e7defc6a7a7bb13c61592b9789a234042c83a51b
bsaglimb <bsaglimb>
2023-12-27 22:43:31 -0500

    added writeToFile function in index

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const fs = require("fs");
 
 // Importing classes from ./lib/shapes directory
 const { Triangle, Square, Circle } = require("./lib/shapes");
+
+// Function writes the SVG file using user answers from inquirer prompts
+function writeToFile(fileName, answers) {
+    // File starts as an empty string
+    let svgString = "";
+    svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  // <g> tag wraps <text> tag so that user font input layers on top of polygon and not behind it
+  svgString += "<g>";
+  // Takes user input for shape choice and inserts it into SVG file
+  svgString += `${answers.shape}`;
+}
