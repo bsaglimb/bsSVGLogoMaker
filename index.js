591774258fb6413636c2a3b7b411139224cecc63
bsaglimb <bsaglimb>
2023-12-27 22:27:06 -0500

    added index js file and some code

---
+++ index.js
@@ -0,0 +1,8 @@
+// Inquirer (node package manager) import
+const inquirer = require("inquirer");
+
+// File system module (node package manager) import
+const fs = require("fs");
+
+// Importing classes from ./lib/shapes directory
+const { Triangle, Square, Circle } = require("./lib/shapes");
